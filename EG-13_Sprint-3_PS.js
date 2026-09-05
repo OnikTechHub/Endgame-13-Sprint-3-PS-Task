9d97d4007671a292452a0dd779abe4a935f405bb
onik-999 <onikdas.dev@gmail.com>
2026-09-05 14:47:23 +0600

    Add solution for problem 09: Longest Consecutive Sequence

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -201,3 +201,34 @@ var topKFrequent = function(nums, k) {
 };
 
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2)); 
+
+
+
+// 09. Longest Consecutive Sequence
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const numSet = new Set(nums);
+    let longestStreak = 0;
+    
+    for (let num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+            
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+            
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+    
+    return longestStreak;
+};
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); 
