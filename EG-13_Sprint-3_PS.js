0ad0c23db07c6d3a67a1f83f2a0b7b51a2a55224
onik-999 <onikdas.dev@gmail.com>
2026-09-05 13:24:32 +0600

    Add solution for problem 02: Move Zeroes

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -10,3 +10,30 @@ var containsDuplicate = function(nums) {
 };
 
 console.log(containsDuplicate([1, 2, 3, 1])); 
+
+
+
+// 02. Move Zeroes
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let insertPos = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[insertPos] = nums[i];
+            insertPos++;
+        }
+    }
+    
+    for (let i = insertPos; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+};
+
+let numsArr = [0, 1, 0, 3, 12];
+moveZeroes(numsArr);
+console.log(numsArr); 
