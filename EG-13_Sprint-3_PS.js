8daadb4fba580ca7b06f2a3b4cfe486bba1af7fe
onik-999 <onikdas.dev@gmail.com>
2026-09-05 14:00:42 +0600

    Add solution for problem 06: 3Sum

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -107,3 +107,44 @@ var majorityElement = function(nums) {
 
 
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); 
+
+
+
+
+
+
+// 06. 3Sum
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let left = i + 1;
+        let right = nums.length - 1;
+        
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return result;
+};
+
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); 
