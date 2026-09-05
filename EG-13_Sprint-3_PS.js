795a68932b41b1eb3935b8a1388cfe4dc8e7b410
onik-999 <onikdas.dev@gmail.com>
2026-09-05 15:05:56 +0600

    Add final solution for problem 10: Sort Colors and complete Sprint-3

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -232,3 +232,35 @@ var longestConsecutive = function(nums) {
 };
 
 console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); 
+
+
+
+
+// 10. Sort Colors
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let low = 0;
+    let mid = 0;
+    let high = nums.length - 1;
+    
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+};
+
+let colorsArr = [2, 0, 2, 1, 1, 0];
+sortColors(colorsArr);
+console.log(colorsArr); 
